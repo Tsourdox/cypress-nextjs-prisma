7138c6f5f84ad81aa3819169557805ec51ce3f1f
David Jensen <tsourdox@icloud.com>
2024-06-14 11:07:02 +0200

    update spec

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,8 +3,13 @@ describe("template spec", () => {
     cy.task("reseed");
   });
 
-  it("removes that post", () => {
+  it("should have the brand inside an h1 tag", () => {
     cy.visit("/");
+    // Om vi skriver så här så säkerställer
+    // vi att det är en h1
+    cy.get("h1").contains("Welcome to my Blog").should("be.visible");
+
+    // Det gör vi inte här, spelar det någon roll?
     cy.getById("title").contains("Welcome to my Blog").should("be.visible");
   });
 });
